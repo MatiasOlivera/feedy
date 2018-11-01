72d93429c19fbab3f6a548436a0003928f510c5d
Matías Olivera <matiasolivera@outlook.com>
2018-11-01 12:28:13 -0300

    Add a simple GraphQL server

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const { NODE_ENV, isDevelopment, SERVER_PORT } = require('./config');
+
+const schema = buildSchema(`
+  type Query {
+    feedback: Feedback
+  }
+
+  type Feedback {
+    id: ID,
+    title: String,
+    description: String
+  }
+`);
+
+const rootValue = {
+  feedback: () => ({
+    id: 1,
+    title: 'Title',
+    description: 'Description'
+  })
+};
+
+const app = express();
+
+const graphqlServer = graphqlHTTP({
+  schema,
+  rootValue,
+  graphiql: isDevelopment
+});
+
+app.use('/graphql', graphqlServer);
+
+app.listen(SERVER_PORT);
+
+/* eslint-disable no-console */
+console.log(`Environment: ${NODE_ENV}. Listening on port ${SERVER_PORT} `);
