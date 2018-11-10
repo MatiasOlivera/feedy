6d9674adb2c5b64eba2489d1ca1d51e7040cc183
Matías Olivera <matiasolivera@outlook.com>
2018-11-09 22:17:09 -0300

    Add database service

---
+++ src/services/db.service.js
@@ -0,0 +1,28 @@
+const Knex = require('knex');
+const Bookshelf = require('bookshelf');
+const {
+  DB_CLIENT,
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_CHARSET,
+  DB_USERNAME,
+  DB_PASSWORD
+} = require('../config');
+
+const knex = Knex({
+  client: DB_CLIENT,
+  connection: {
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_DATABASE,
+    charset: DB_CHARSET,
+    user: DB_USERNAME,
+    password: DB_PASSWORD
+  }
+});
+
+const bookshelf = Bookshelf(knex);
+bookshelf.plugin('registry');
+
+module.exports = bookshelf;
