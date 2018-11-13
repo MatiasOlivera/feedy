8431984439d977f1fd44710fe9e2fe3bc0ee8deb
Matías Olivera <matiasolivera@outlook.com>
2018-11-13 20:45:17 -0300

    Add Objection.js config

--- src/app.js
+++ src/app.js
@@ -1,40 +1,29 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
-const { NODE_ENV, isDevelopment, SERVER_PORT } = require('./config');
+const { isDevelopment, SERVER_PORT } = require('./config');
+const initDBService = require('./services/db.service');
 const schema = require('./graphql');
 
-const schema = buildSchema(`
-  type Query {
-    feedback: Feedback
+async function initServer() {
+  try {
+    await initDBService();
+
+    const app = express();
+
+    const graphqlServer = graphqlHTTP({
+      schema,
+      graphiql: isDevelopment
+    });
+
+    app.use('/graphql', graphqlServer);
+
+    app.listen(SERVER_PORT);
+
+    // eslint-disable-next-line no-console
+    console.log(`Server running on http://localhost:${SERVER_PORT}/graphql 🔥`);
+  } catch (err) {
+    throw err;
   }
+}
 
-  type Feedback {
-    id: ID,
-    title: String,
-    description: String
-  }
-`);
-
-const rootValue = {
-  feedback: () => ({
-    id: 1,
-    title: 'Title',
-    description: 'Description'
-  })
-};
-
-const app = express();
-
-const graphqlServer = graphqlHTTP({
-  schema,
-  rootValue,
-  graphiql: isDevelopment
-});
-
-app.use('/graphql', graphqlServer);
-
-app.listen(SERVER_PORT);
-
-/* eslint-disable no-console */
-console.log(`Environment: ${NODE_ENV}. Listening on port ${SERVER_PORT} `);
+initServer();
--- src/services/db.service.js
+++ src/services/db.service.js
@@ -1,4 +1,5 @@
 const Knex = require('knex');
+const { Model } = require('objection');
 const {
   DB_CLIENT,
   DB_HOST,
@@ -9,14 +10,31 @@ const {
   DB_PASSWORD
 } = require('../config');
 
-const knex = Knex({
-  client: DB_CLIENT,
-  connection: {
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_DATABASE,
-    charset: DB_CHARSET,
-    user: DB_USERNAME,
-    password: DB_PASSWORD
-  }
-});
+function initDBService() {
+  // Create a new knex instance
+  const knex = Knex({
+    client: DB_CLIENT,
+    connection: {
+      host: DB_HOST,
+      port: DB_PORT,
+      database: DB_DATABASE,
+      charset: DB_CHARSET,
+      user: DB_USERNAME,
+      password: DB_PASSWORD
+    }
+  });
+
+  return knex
+    .raw('select true')
+    .then(() => {
+      // Bind all models to a knex instance
+      Model.knex(knex);
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+module.exports = initDBService;
