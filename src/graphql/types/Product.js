2f4fc82f3589126a9ba27ecadcc96c35d4d83ac7
Matías Olivera <matiasolivera@outlook.com>
2018-11-18 17:00:37 -0300

    Add Product resolver

---
+++ src/graphql/types/Product.js
@@ -0,0 +1,25 @@
+/* eslint-disable func-names */
+
+const { Product } = require('../../models');
+
+const owner = async function(parent) {
+  const product = await Product.query()
+    .findById(parent.id)
+    .eager('owner.[user, organization]');
+  const { user, organization } = product.owner;
+
+  return user || organization;
+};
+
+const issues = async function(parent) {
+  const product = await Product.query()
+    .findById(parent.id)
+    .eager('issues');
+
+  return product.issues;
+};
+
+module.exports = {
+  owner,
+  issues
+};
--- src/graphql/types/index.js
+++ src/graphql/types/index.js
@@ -1,9 +1,11 @@
 const Organization = require('./Organization');
+const Product = require('./Product');
 const ProductOwner = require('./ProductOwner');
 const User = require('./User');
 
 module.exports = {
   Organization,
+  Product,
   ProductOwner,
   User
 };
