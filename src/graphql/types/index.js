7806c6b6030c7d441e63186a2517bb9d3a10badb
Matías Olivera <matiasolivera@outlook.com>
2018-11-18 17:45:35 -0300

    Add Issue resolver

--- src/graphql/types/index.js
+++ src/graphql/types/index.js
@@ -1,9 +1,11 @@
+const Issue = require('./Issue');
 const Organization = require('./Organization');
 const Product = require('./Product');
 const ProductOwner = require('./ProductOwner');
 const User = require('./User');
 
 module.exports = {
+  Issue,
   Organization,
   Product,
   ProductOwner,
