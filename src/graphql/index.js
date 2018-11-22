a2cec618a182a4755d6887140f533cdea4ddf560
Matías Olivera <matiasolivera@outlook.com>
2018-11-21 23:02:48 -0300

    Add logs to graphql resolvers

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -5,6 +5,7 @@ const { maskErrors } = require('graphql-errors');
 const Query = require('./queries');
 const Types = require('./types');
 const Scalars = require('./scalars');
+const { logger } = require('../services/log.service');
 
 const schemaFile = path.join(__dirname, 'schema.graphql');
 const typeDefs = fs.readFileSync(schemaFile, 'utf8');
@@ -15,7 +16,16 @@ const resolvers = {
   ...Scalars
 };
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const loggerHandler = {
+  log: (err) =>
+    logger.error('[gql service] Error in a resolver function', { error: err })
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  logger: loggerHandler
+});
 
 // Mask the error messages
 maskErrors(schema);
