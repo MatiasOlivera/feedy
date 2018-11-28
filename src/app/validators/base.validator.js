2167989a1d347af9203000befd2e4fd6f9e396d2
Matías Olivera <matiasolivera@outlook.com>
2018-11-28 14:27:07 -0300

    Add base validator to handle validation logic
    - Allows to register custom rules
    - Allows you to format errors
    - Use the promise api in sync and async validations

---
+++ src/app/validators/base.validator.js
@@ -0,0 +1,61 @@
+const Validator = require('validatorjs');
+const customRules = require('./custom_rules');
+
+class BaseValidator {
+  constructor() {
+    this.errors = null;
+    this.registerCustomRules();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  registerCustomRules() {
+    const { uniqueRule } = customRules;
+
+    Validator.registerAsync(uniqueRule.name, uniqueRule.callback);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  rules() {
+    return {};
+  }
+
+  validate(data) {
+    return new Promise((resolve, reject) => {
+      const validator = new Validator(data, this.rules());
+      const handleFails = () => {
+        this.errors = validator.errors.all();
+        reject(this.formattedErrors);
+      };
+
+      // Asynchronous handler
+      if (validator.hasAsync) {
+        validator.passes(() => resolve());
+        validator.fails(() => handleFails());
+      } else {
+        // Synchronous handler
+        // eslint-disable-next-line no-lonely-if
+        if (validator.passes()) {
+          resolve();
+        } else {
+          handleFails();
+        }
+      }
+    });
+  }
+
+  get formattedErrors() {
+    if (!this.errors) return null;
+
+    const keys = Object.keys(this.errors);
+    const formattedErrors = { ...this.errors };
+
+    keys.forEach((key) => {
+      const [message] = this.errors[key];
+      formattedErrors[key] = message;
+    });
+
+    return formattedErrors;
+  }
+}
+
+module.exports = BaseValidator;
