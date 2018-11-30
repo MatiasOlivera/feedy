bfb24a6fd27f4a8ef9f12b309fd095c4dcbf1c8f
Matías Olivera <matiasolivera@outlook.com>
2018-11-30 10:09:07 -0300

    Add mutation to delete an user

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,7 +1,9 @@
 const createUser = require('./users/create.user.mutation');
 const updateUser = require('./users/update.user.mutation');
+const deleteUser = require('./users/delete.user.mutation');
 
 module.exports = {
   createUser,
-  updateUser
+  updateUser,
+  deleteUser
 };
