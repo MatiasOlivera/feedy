3b17723accadffc9cdb0e83acde99c69df9ed5c6
Matías Olivera <matiasolivera@outlook.com>
2018-12-01 15:21:09 -0300

    Add mutation to create an organization

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,9 +1,11 @@
 const createUser = require('./users/create.user.mutation');
 const updateUser = require('./users/update.user.mutation');
 const deleteUser = require('./users/delete.user.mutation');
+const createOrganization = require('./organizations/create.org.mutation');
 
 module.exports = {
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  createOrganization
 };
