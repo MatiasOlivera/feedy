f47ee91df8c23428153ba7ada9ab5b3c849bc12c
Matías Olivera <matiasolivera@outlook.com>
2018-12-02 20:27:06 -0300

    Add create product validator

--- src/app/validators/index.js
+++ src/app/validators/index.js
@@ -2,10 +2,12 @@ const CreateUserValidator = require('./users/create.user.validator');
 const UpdateUserValidator = require('./users/update.user.validator');
 const CreateOrganizationValidator = require('./organizations/create.org.validator');
 const UpdateOrganizationValidator = require('./organizations/update.org.validator');
+const CreateProductValidator = require('./products/create.product.validator');
 
 module.exports = {
   CreateUserValidator,
   UpdateUserValidator,
   CreateOrganizationValidator,
-  UpdateOrganizationValidator
+  UpdateOrganizationValidator,
+  CreateProductValidator
 };
