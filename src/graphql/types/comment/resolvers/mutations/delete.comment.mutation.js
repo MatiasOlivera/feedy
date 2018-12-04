3a47bb4788bd1c33777f304e7139bc34cfbf825f
Matías Olivera <matiasolivera@outlook.com>
2018-12-04 17:39:04 -0300

    Add mutation to delete a comment

---
+++ src/graphql/types/comment/resolvers/mutations/delete.comment.mutation.js
@@ -0,0 +1,43 @@
+const objection = require('objection');
+const { knex } = require('../../../../../services/db.service');
+const { Comment } = require('../../../../../models');
+
+const deleteComment = async (root, args) => {
+  let comment;
+
+  try {
+    comment = await Comment.query().findById(args.id);
+
+    if (!comment)
+      return {
+        operation: { status: false, message: 'The comment does not exists' },
+        comment: null
+      };
+  } catch (err) {
+    throw err;
+  }
+
+  const tsx = await objection.transaction.start(knex);
+  try {
+    await comment
+      .$relatedQuery('issue', tsx)
+      .unrelate()
+      .where('comment_id', args.id);
+    await Comment.query(tsx).deleteById(args.id);
+
+    await tsx.commit();
+
+    return {
+      operation: {
+        status: true,
+        message: 'The comment was deleted succesfully'
+      },
+      comment
+    };
+  } catch (err) {
+    await tsx.rollback();
+    throw err;
+  }
+};
+
+module.exports = { Mutation: { deleteComment } };
