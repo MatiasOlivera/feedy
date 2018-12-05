a949f347a383ed40561fb9ebafd487c2859dd4f1
Matías Olivera <matiasolivera@outlook.com>
2018-12-05 20:29:18 -0300

    Update the model timestamps automatically
    Fixes #2

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,6 +1,14 @@
 const { Model: ObjectionModel, snakeCaseMappers } = require('objection');
 
 class Model extends ObjectionModel {
+  $beforeInsert() {
+    this.created_at = new Date();
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date();
+  }
+
   static get columnNameMappers() {
     return snakeCaseMappers();
   }
