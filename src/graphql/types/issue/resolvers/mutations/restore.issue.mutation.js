d7d3410798a890c64ab6e7d3b2b837693372303f
Matías Olivera <matiasolivera@outlook.com>
2018-12-08 21:39:43 -0300

    Add mutation to restore an issue

---
+++ src/graphql/types/issue/resolvers/mutations/restore.issue.mutation.js
@@ -0,0 +1,38 @@
+const { Issue } = require('../../../../../models');
+
+const restoreIssue = async (root, args) => {
+  try {
+    const issue = await Issue.query().findById(args.id);
+
+    if (!issue)
+      return {
+        operation: {
+          status: false,
+          message: 'The issue does not exists'
+        },
+        issue: null
+      };
+  } catch (err) {
+    throw err;
+  }
+
+  try {
+    await Issue.query()
+      .where('id', args.id)
+      .restore();
+
+    const issue = await Issue.query().findById(args.id);
+
+    return {
+      operation: {
+        status: true,
+        message: 'The issue was restored succesfully'
+      },
+      issue
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { Mutation: { restoreIssue } };
