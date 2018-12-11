a5831e4292d2fba0d761753792a351c53bd2b37b
Matías Olivera <matiasolivera@outlook.com>
2018-12-11 14:44:56 -0300

    Call paginate function in queries resolvers

--- src/graphql/queries/comments/comments/comments.query.js
+++ src/graphql/queries/comments/comments/comments.query.js
@@ -1,27 +1,9 @@
 const { Comment } = require('../../../../models');
-const {
-  validatePaginationArgs,
-  getPage,
-  getColumn
-} = require('../../../../services/db.service');
+const paginate = require('../../_utils/pagination');
 
 const commentsQuery = async (root, args) => {
   try {
-    const { page, limit, orderBy, direction, deleted } = args;
-
-    const { columns } = await Comment.fetchTableMetadata();
-    const column = getColumn(orderBy);
-    validatePaginationArgs(page, limit, { column, columns });
-
-    const pageNumber = getPage(page);
-
-    let query = Comment.query();
-    query = deleted ? query.whereDeleted() : query.whereNotDeleted();
-    query.orderBy(column, direction).page(pageNumber, limit);
-
-    const comments = await query;
-
-    return comments.results;
+    return await paginate(Comment, args);
   } catch (err) {
     throw err;
   }
--- src/graphql/queries/issues/issues/issues.query.js
+++ src/graphql/queries/issues/issues/issues.query.js
@@ -1,27 +1,9 @@
 const { Issue } = require('../../../../models');
-const {
-  validatePaginationArgs,
-  getPage,
-  getColumn
-} = require('../../../../services/db.service');
+const paginate = require('../../_utils/pagination');
 
 const issuesQuery = async (root, args) => {
   try {
-    const { page, limit, orderBy, direction, deleted } = args;
-
-    const { columns } = await Issue.fetchTableMetadata();
-    const column = getColumn(orderBy);
-    validatePaginationArgs(page, limit, { column, columns });
-
-    const pageNumber = getPage(page);
-
-    let query = Issue.query();
-    query = deleted ? query.whereDeleted() : query.whereNotDeleted();
-    query.orderBy(column, direction).page(pageNumber, limit);
-
-    const issues = await query;
-
-    return issues.results;
+    return await paginate(Issue, args);
   } catch (err) {
     throw err;
   }
