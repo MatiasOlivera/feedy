7b1ab8de19196f7043076358f9c180ee9d617b17
Matías Olivera <matiasolivera@outlook.com>
2018-12-15 13:31:25 -0300

    Fix typo on user type
    Fixes 1fcb81fd9a8707b0b8db5124af600b689eead19f

--- src/graphql/types/user/user.type.ts
+++ src/graphql/types/user/user.type.ts
@@ -1,13 +1,13 @@
 import { isEmptyReturnNull } from '../_utils';
 import { Product, Organization, Issue, Comment } from '../../../models';
-import { User } from 'graphql-schema';
+import { User as IUser } from 'graphql-schema';
 
-async function products(user: User) {
+async function products(user: IUser) {
   const rows = await Product.query().where('owner_id', user.id);
   return isEmptyReturnNull(rows);
 }
 
-async function organizations(user: User) {
+async function organizations(user: IUser) {
   const rows = await Organization.query()
     .joinRelation('members')
     .where('user_id', user.id);
@@ -15,12 +15,12 @@ async function organizations(user: User) {
   return isEmptyReturnNull(rows);
 }
 
-async function issues(user: User) {
+async function issues(user: IUser) {
   const rows = await Issue.query().where('user_id', user.id);
   return isEmptyReturnNull(rows);
 }
 
-async function comments(user: User) {
+async function comments(user: IUser) {
   const rows = await Comment.query().where('user_id', user.id);
   return isEmptyReturnNull(rows);
 }
