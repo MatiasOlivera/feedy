67040d6c41d370722d769a58290418ab2e12ec4a
Matías Olivera <matiasolivera@outlook.com>
2018-12-16 20:43:15 -0300

    Move type definitions to index.d.ts

---
+++ server/src/index.d.ts
@@ -0,0 +1,121 @@
+// Type definitions for the GraphQL schema resolvers
+declare module 'graphql-schema' {
+  export interface IModel {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date;
+  }
+
+  export interface IUser extends IModel {
+    firstName: string;
+    lastName: string;
+    fullName: string;
+    gender: string;
+    username: string;
+    email: string;
+    bio: string;
+  }
+
+  export interface IUserSimplePayload {
+    operation: IOperation;
+    user: IUser;
+  }
+
+  export interface IUserPayload extends IUserSimplePayload {
+    errors: IValidationErrors;
+  }
+
+  export interface IOrganization extends IModel {
+    name: string;
+    bio: string;
+  }
+
+  export interface IOrganizationSimplePayload {
+    operation: IOperation;
+    organization: IOrganization;
+  }
+
+  export interface IOrganizationPayload extends IOrganizationSimplePayload {
+    errors: IValidationErrors;
+  }
+
+  export interface IProduct extends IModel {
+    name: string;
+    description: string;
+    ownerId: string;
+  }
+
+  export interface IProductSimplePayload {
+    operation: IOperation;
+    product: IProduct;
+  }
+
+  export interface IProductPayload extends IProductSimplePayload {
+    errors: IValidationErrors;
+  }
+
+  export interface IIssue extends IModel {
+    title: string;
+    body: string;
+    userId: string;
+    productId: string;
+  }
+
+  export interface IIssueSimplePayload {
+    operation: IOperation;
+    issue: IIssue;
+  }
+
+  export interface IIssuePayload extends IIssueSimplePayload {
+    errors: IValidationErrors;
+  }
+
+  export interface IComment extends IModel {
+    body: string;
+    userId: string;
+    issueId: string;
+    parentId: string;
+  }
+
+  export interface ICommentSimplePayload {
+    operation: IOperation;
+    comment: IComment;
+  }
+
+  export interface ICommentPayload extends ICommentSimplePayload {
+    errors: IValidationErrors;
+  }
+
+  export interface IPagination {
+    page: number;
+    limit: number;
+    orderBy: string;
+    direction: 'ASC' | 'DESC';
+    deleted: boolean;
+  }
+
+  export interface IOperation {
+    status: boolean;
+    message: string;
+  }
+
+  export interface IValidationErrors {
+    [attribute: string]: string;
+  }
+}
+
+// Type definitions for graphql-errors
+declare module 'graphql-errors' {
+  import { GraphQLSchema } from 'graphql';
+
+  type handlerFunction = (err: Error) => Error;
+
+  export function setDefaultHandler(fn: handlerFunction): void;
+
+  export function maskErrors(schema: GraphQLSchema, fn?: handlerFunction): void;
+
+  export class UserError extends Error {
+    constructor(message: string);
+  }
+}
