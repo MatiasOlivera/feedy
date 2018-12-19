fab9a529a1d7bd467ef8b07118a613c84f5bd68f
Matías Olivera <matiasolivera@outlook.com>
2018-12-19 20:51:51 -0300

    Add TheHeader and RouterView to App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>App</h1>
+  <div>
+    <the-header />
+
+    <div class="section">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+// Components
+import TheHeader from './components/TheHeader.vue';
+
+@Component({
+  components: { TheHeader }
+})
+export default class App extends Vue {}
+</script>
+
+<style scoped>
+</style>
