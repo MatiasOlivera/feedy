ee9f489307bf412ca53cb46749194407fb9670cf
Matías Olivera <matiasolivera@outlook.com>
2019-01-04 19:42:47 -0300

    Add types to issue validators

--- server/src/app/validators/issues/issue.validator.ts
+++ server/src/app/validators/issues/issue.validator.ts
@@ -1,13 +1,13 @@
 import BaseValidator from '../base.validator';
 
-class IssueValidator extends BaseValidator {
+class IssueValidator<T> extends BaseValidator<T> {
   // eslint-disable-next-line class-methods-use-this
   rules() {
     return {
       title: ['string', 'between:3,50'],
       body: ['string', 'max:255'],
-      userId: ['integer', 'exists:users,id'],
-      productId: ['integer', 'exists:products,id']
+      userId: ['alpha_num', 'exists:user,id'],
+      productId: ['alpha_num', 'exists:product,id']
     };
   }
 }
