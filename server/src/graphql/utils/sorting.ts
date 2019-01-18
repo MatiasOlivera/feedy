63d0b096da44623c893b13b6d1e6ee5eea9f27c8
Matías Olivera <matiasolivera@outlook.com>
2019-01-18 20:13:06 -0300

    Make sure that the values exist before sorting
    Avoid return an invalid value. Eg: "field_undefined"

--- server/src/graphql/utils/sorting.ts
+++ server/src/graphql/utils/sorting.ts
@@ -1,6 +1,20 @@
+import { UserError } from 'graphql-errors';
+
 export function getSortingArguments<Enum extends string>(
   orderBy: OrderBy
 ): Enum {
+  if (!orderBy) {
+    return null;
+  }
+
+  if (!orderBy.field) {
+    throw new UserError('Sorting: Field must be specified');
+  }
+
+  if (!orderBy.direction) {
+    throw new UserError('Sorting: Direction must be specified');
+  }
+
   return (`${orderBy.field}_${orderBy.direction}` as unknown) as Enum;
 }
 
