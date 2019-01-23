e8f1c67307ccb66c06317102331358e6a0b8336f
Matías Olivera <matiasolivera@outlook.com>
2019-01-23 10:31:55 -0300

    Refactor getPaginationArguments

--- server/src/graphql/utils/pagination.ts
+++ server/src/graphql/utils/pagination.ts
@@ -8,52 +8,63 @@ export function getPaginationArguments(
 ): PaginationType {
   const itemsPerPage = 10;
   const defaultPagination = { first: itemsPerPage };
+  const limits = { minLimit: 0, maxLimit: 20 };
 
   // If default pagination was not specified in the schema
   // return a fallback response
   if (!pagination || isEmpty(pagination)) return defaultPagination;
 
   const { first, after, last, before } = pagination;
-  const minLimit = 0;
-  const maxLimit = 20;
 
-  if (first || first === minLimit) {
-    if (first <= minLimit) {
-      throw new UserError(`First must be a number greater than ${minLimit}`);
+  try {
+    if (validateLimit('First', first, limits)) {
+      return { first, after };
     }
 
-    if (first > maxLimit) {
-      throw new UserError(
-        `First must be a number smaller or equal to ${maxLimit}`
-      );
+    if (after) {
+      return { first: itemsPerPage, after };
     }
-
-    return { first, after };
+  } catch (err) {
+    throw err;
   }
 
-  if (after) {
-    return { first: itemsPerPage, after };
-  }
-
-  if (last || last === minLimit) {
-    if (last <= minLimit) {
-      throw new UserError(`Last must be a number greater than ${minLimit}`);
+  try {
+    if (validateLimit('Last', last, limits)) {
+      return { last, before };
     }
 
-    if (last > maxLimit) {
-      throw new UserError(
-        `Last must be a number smaller or equal to ${maxLimit}`
-      );
+    if (before) {
+      return { last: itemsPerPage, before };
     }
-
-    return { last, before };
-  }
-
-  if (before) {
-    return { last: itemsPerPage, before };
+  } catch (err) {
+    throw err;
   }
 
   return defaultPagination;
+}
+
+function validateLimit(
+  fieldName: string,
+  limit: number,
+  limits: Limits
+): boolean {
+  const { minLimit, maxLimit } = limits;
+
+  if (!(limit || limit === minLimit)) {
+    return false;
+  }
+
+  if (limit <= minLimit) {
+    const msg = `${fieldName} must be a number greater than ${minLimit}`;
+    throw new UserError(msg);
+  }
+
+  if (limit > maxLimit) {
+    const msg = `${fieldName} must be a number smaller or equal to ${maxLimit}`;
+    throw new UserError(msg);
+  }
+
+  return true;
 }
 
 type Forward = {
@@ -67,3 +78,8 @@ type Backward = {
 };
 
 type PaginationType = Forward | Backward;
+
+interface Limits {
+  minLimit: number;
+  maxLimit: number;
+}
